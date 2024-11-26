06ed398f2a2317556f7382a6b3235821a59b553d
Milo Sones <MiloSones@users.noreply.github.com>
2024-11-26 14:19:39 +0000

    Add placeholder functions

---
+++ api.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_posts():
+    pass
+
+def get_comments(post_id):
+    pass
+
+def post_comment(comment):
+    pass
---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+from api import get_posts, get_comments, post_comment
+
+def view_posts_menu():
+    pass
+
+def view_post_menu():
+    pass
+
+def view_comments_menu():
+    pass
+
+def view_random_posts():
+    pass
+
+def view_top_posts():
+    pass
+
+def view_comments():
+    pass
+
