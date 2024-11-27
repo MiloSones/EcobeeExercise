5b5337c9f90e895ac2a9d07feccd06b64085216d
Milo Sones <MiloSones@users.noreply.github.com>
2024-11-27 15:26:50 +0000

    Add get all comments function

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ TIMEOUT = 5
 
 def get_posts():
     try:
-        r = requests.get(BASE_URL + "posts", timeout=TIMEOUT)
+        r = requests.get(BASE_URL + "posts/", timeout=TIMEOUT)
         if r.status_code == 200:
             return r.json()
 
@@ -14,9 +14,20 @@ def get_posts():
     except requests.exceptions.Timeout:
         print("Timed out")
 
-def get_comments(post_id):
+def get_posts_comments(post_id):
     try:
         r = requests.get(BASE_URL + f"posts/{post_id}/comments", timeout=TIMEOUT)
+        if r.status_code == 200:
+            return r.json()
+
+        print(f"Status code: {r.status_code}")
+        return 0
+    except requests.exceptions.Timeout:
+        print("Timed out")
+
+def get_all_comments(post_id):
+    try:
+        r = requests.get(BASE_URL + "comments", timeout=TIMEOUT)
         if r.status_code == 200:
             return r.json()
 
