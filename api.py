902571bdd05f2a79649ab997a3333e8a1df50bbd
Milo Sones <MiloSones@users.noreply.github.com>
2024-11-27 14:06:33 +0000

    Add post requests implementation

--- api.py
+++ api.py
@@ -1,10 +1,29 @@
 import requests
 
+BASE_URL = "https://jsonplaceholder.typicode.com/"
+TIMEOUT = 10
+
 def get_posts():
-    pass
+    try:
+        r = requests.get(BASE_URL + "posts", timeout=TIMEOUT)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            print(f"Status code: {r.status_code}")
+            return 0
+    except requests.exceptions.Timeout:
+        print("Timed out")
 
 def get_comments(post_id):
-    pass
+    try:
+        r = requests.get(BASE_URL + f"comments/{post_id}", timeout=TIMEOUT)
+        if r.status_code == 200:
+            return r.json()
+        else:
+            print(f"Status code: {r.status_code}")
+            return 0
+    except requests.exceptions.Timeout:
+        print("Timed out")
 
 def post_comment(comment):
-    pass
+    return 0
