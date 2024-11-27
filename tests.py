ba7dfdebe4454b81d294bd486d87812624da73af
Milo Sones <MiloSones@users.noreply.github.com>
2024-11-27 13:58:37 +0000

    Add unit tests for user input validation

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch, MagicMock
 from api import get_posts, get_comments, post_comment
-from main import view_comments
+from main import view_comments, view_posts_menu, view_random_posts
 from io import StringIO
 
 class TestApi(unittest.TestCase):
@@ -11,7 +11,6 @@ class TestApi(unittest.TestCase):
         mock_response = MagicMock()
         mock_data = [{'userId': 1, 'id': 1, 'title': 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', 'body': 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'}, {'userId': 1, 'id': 2, 'title': 'qui est esse', 'body': 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'}, {'userId': 1, 'id': 3, 'title': 'ea molestias quasi exercitationem repellat qui ipsa sit aut', 'body': 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'}]
         mock_response.json.return_value = mock_data
-        mock_response.raise_for_status = MagicMock()
         mock_get.return_value = mock_response
 
         result = get_posts()
@@ -22,26 +21,26 @@ class TestApi(unittest.TestCase):
         mock_response = MagicMock()
         mock_data = [{'postId': 1, 'id': 1, 'name': 'id labore ex et quam laborum', 'email': 'Eliseo@gardner.biz', 'body': 'laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium'}, {'postId': 1, 'id': 2, 'name': 'quo vero reiciendis velit similique earum', 'email': 'Jayne_Kuhic@sydney.com', 'body': 'est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et'}]
         mock_response.json.return_value = mock_data
-        mock_response.raise_for_status = MagicMock()
         mock_get.return_value = mock_response
 
-        result = get_comments()
+        result = get_comments(1)
         self.assertEqual(result, mock_data)
 
     @patch('api.requests.post')
     def test_post_comment(self, mock_post):
         mock_response = MagicMock()
         mock_response.status_code = 201
-        mock_response.json.return_value = {
+        mock_data = {
             'id': 101,
             'postId': 1,
             'name': 'test name',
             'email': 'test@example.com',
             'body': 'test comment'
         }
+        mock_response.json.return_value = mock_data
         mock_post.return_value = mock_response
 
-        result = post_comment(1, 'test name', 'test@example.com', 'test comment')
+        result = post_comment(mock_data)
 
 
         self.assertEqual(result, mock_response.json.return_value)
@@ -66,17 +65,66 @@ class TestPrint(unittest.TestCase):
         view_comments()
 
         output = mock_stdout.getvalue()
-        expected_output ="""[Eliseo@gardner.biz] id labore ex et quam laborum\n
-                            laudantium enim quasi est quidem magnam voluptate ipsam eos\n
-                            tempora quo necessitatibus\n
-                            dolor quam autem quasi\n
-                            reiciendis et nam sapiente accusantium\n
-                            \n
-                            [Jayne_Kuhic@sydney.com] quo vero reiciendis velit similique earum\n
-                            est natus enim nihil est dolore omnis voluptatem numquam\n
-                            et omnis occaecati quod ullam at\n
-                            voluptatem error expedita pariatur\n
-                            nihil sint nostrum voluptatem reiciendis et\n
-                        """
+        expected_output =   ("[Eliseo@gardner.biz] id labore ex et quam laborum\n"
+                            "laudantium enim quasi est quidem magnam voluptate ipsam eos\n"
+                            "tempora quo necessitatibus\n"
+                            "dolor quam autem quasi\n"
+                            "reiciendis et nam sapiente accusantium\n"
+                            "\n"
+                            "[Jayne_Kuhic@sydney.com] quo vero reiciendis velit similique earum\n"
+                            "est natus enim nihil est dolore omnis voluptatem numquam\n"
+                            "et omnis occaecati quod ullam at\n"
+                            "voluptatem error expedita pariatur\n"
+                            "nihil sint nostrum voluptatem reiciendis et\n")
         self.assertEqual(output, expected_output)
         mock_fetch.assert_called_once()
+
+class TestUserValidation(unittest.TestCase):
+    @patch('sys.stdout', new_callable=StringIO)
+    @patch('builtins.input', return_value="a")
+    def test_nonnumeric_input(self, mock_stdout):
+        
+        view_posts_menu()
+
+        self.assertEqual(mock_stdout.getvalue(),
+        "Choose an option:\n"
+        "1. View 10 random post titles\n"
+        "2. View 10 most commented post titles\n"
+        "3. exit\n"
+        "Enter your choice (1-3): a\n"
+        "Please enter a number between 1 and 3\n"
+        "\n"
+        "Choose an option:\n"
+        "1. View 10 random post titles\n"
+        "2. View 10 most commented post titles\n"
+        "3. exit\n"
+        "Enter your choice (1-3): ")
+
+
+    @patch('sys.stdout', new_callable=StringIO)
+    @patch('builtins.input', return_value="4")
+    def test_out_of_bounds(self, mock_stdout):
+        
+        view_posts_menu()
+
+        self.assertEqual(mock_stdout.getvalue(),
+        "Choose an option:\n"
+        "1. View 10 random post titles\n"
+        "2. View 10 most commented post titles\n"
+        "3. exit\n"
+        "Enter your choice (1-3): 4\n"
+        "Please enter a number between 1 and 3\n"
+        "\n"
+        "Choose an option:\n"
+        "1. View 10 random post titles\n"
+        "2. View 10 most commented post titles\n"
+        "3. exit\n"
+        "Enter your choice (1-3): ")
+
+    @patch('main.view_random_posts')
+    @patch('builtins.input', return_value="1")
+    def test_valid_input(self, mock_called_function):
+        
+        view_posts_menu()
+
+        mock_called_function.assert_called_once()
