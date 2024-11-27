c2b63c1cd6d1606f7c7bcba0abd0f90730f14b76
Milo Sones <MiloSones@users.noreply.github.com>
2024-11-27 14:14:07 +0000

    Fix url path

--- api.py
+++ api.py
@@ -1,29 +1,37 @@
 import requests
 
 BASE_URL = "https://jsonplaceholder.typicode.com/"
-TIMEOUT = 10
+TIMEOUT = 5
 
 def get_posts():
     try:
         r = requests.get(BASE_URL + "posts", timeout=TIMEOUT)
         if r.status_code == 200:
             return r.json()
-        else:
-            print(f"Status code: {r.status_code}")
-            return 0
+
+        print(f"Status code: {r.status_code}")
+        return 0
     except requests.exceptions.Timeout:
         print("Timed out")
 
 def get_comments(post_id):
     try:
-        r = requests.get(BASE_URL + f"comments/{post_id}", timeout=TIMEOUT)
+        r = requests.get(BASE_URL + f"posts/{post_id}/comments", timeout=TIMEOUT)
         if r.status_code == 200:
             return r.json()
-        else:
-            print(f"Status code: {r.status_code}")
-            return 0
+
+        print(f"Status code: {r.status_code}")
+        return 0
     except requests.exceptions.Timeout:
         print("Timed out")
 
-def post_comment(comment):
-    return 0
+def post_comment(post_id,comment):
+    try:
+        r = requests.post(BASE_URL + f"posts/{post_id}/comments",json=, timeout=TIMEOUT)
+        if r.status_code == 201:
+            return r.json()
+
+        print(f"Status code: {r.status_code}")
+        return 0
+    except requests.exceptions.Timeout:
+        print("Timed out")
